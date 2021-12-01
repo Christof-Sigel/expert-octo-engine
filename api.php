b9d6128e5bec1276aea188e88280ef9ed9bfa69a
Christof Sigel <christof.sigel.work@gmail.com>
2021-12-01 19:19:02 +1030

    Sketch out the API signature and make sure we highlight errors.

--- api.php
+++ api.php
@@ -5,6 +5,9 @@ require 'carbon/autoload.php';
 use Carbon\Carbon;
 use Carbon\CarbonInterval;
 
+http_response_code(400);
+die("There was an error");
+
 printf("Now: %s", Carbon::now());
 
 printf("1 day: %s", CarbonInterval::day()->forHumans());
